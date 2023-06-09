89169692ac525f4eaace1c96ecf148d48281e8fb
Trevor Gardner <trevorthebike@gmail.com>
2023-06-08 21:07:53 -0700

    added dorothy animation

--- src/yellow.js
+++ src/yellow.js
@@ -6,15 +6,23 @@ class Yellow extends Phaser.Scene{
 
     preload(){
         this.load.path = './assets/';
-        this.load.spritesheet('slime', 'png/slime.png', {
+        this.load.spritesheet('dorothy', 'png/dorothy.png', {
             frameWidth: 16,
             frameHeight: 16
         })
         this.load.spritesheet('lion', 'png/lion.png', {
           frameWidth: 64,
           frameHeight: 64
-      })
-        this.load.image('tinman', 'png/tinman.png');
+        })
+        this.load.spritesheet('tinman', 'png/tinrobot.png', {
+          frameWidth: 28,
+          frameHeight: 36
+        })
+        /*this.load.spritesheet('scarecrow', 'png/scarecrow.png', {
+          frameWidth: 32,
+          frameHeight: 32
+        })*/
+        //this.load.image('tinman', 'png/tinman.png');
         this.load.image('scarecrow', 'png/ScareCrow.png');
         //this.load.image('lion', 'png/lion.png');
         this.load.image('tilesetImage', 'png/tileset.png');
@@ -26,53 +34,96 @@ class Yellow extends Phaser.Scene{
         const tileset = map.addTilesetImage('tileset', 'tilesetImage');
         const bgLayer = map.createLayer('Background', tileset, 0, 0);
         const treeLayer = map.createLayer('Trees', tileset, 0, 0);
-        this.slime = this.physics.add.sprite(32, 32, 'slime', 0);
         this.tinman = this.physics.add.sprite(320, 320, 'tinman', 0);
         this.scarecrow = this.physics.add.sprite(320, 32, 'scarecrow', 0);
         this.lion = this.physics.add.sprite(100, 320, 'lion', 4);
+        this.dorothy = this.physics.add.sprite(32, 32, 'dorothy', 0);
+        function createAnimations(spriteKey, animations) {
+          animations.forEach(animation => {
+            this.anims.create({
+              key: animation.key,
+              frameRate: 6,
+              repeat: -1,
+              frames: this.anims.generateFrameNumbers(spriteKey, animation.frames)
+            });
+          });
+        }
+        
+        const lionAnimations = [
+          {
+            key: 'lionwalkfront',
+            frames: { start: 6, end: 8 }
+          },
+          {
+            key: 'lionwalkback',
+            frames: { start: 0, end: 2 }
+          },
+          {
+            key: 'lionwalkright',
+            frames: { start: 3, end: 5 }
+          },
+          {
+            key: 'lionwalkleft',
+            frames: { start: 9, end: 11 }
+          }
+        ];
+        createAnimations.call(this, 'lion', lionAnimations);
+        const tinmanAnimations = [
+          {
+            key: 'tinmanfront',
+            frames: { start: 0, end: 2 }
+          },
+          {
+            key: 'tinmanback',
+            frames: { start: 3, end: 5 }
+          },
+          {
+            key: 'tinmanright',
+            frames: { start: 6, end: 8 }
+          },
+          {
+            key: 'tinmanleft',
+            frames: { start: 9, end: 11 }
+          }
+        ];
+        createAnimations.call(this, 'tinman', tinmanAnimations);
         this.anims.create({
-          key: 'lionwalkfront', 
-          frameRate: 6,
+          key: 'dorothyfront', 
+          frameRate: 4,
           repeat: -1,
-          frames: this.anims.generateFrameNumbers('lion', { start: 6, end: 8 })
+          frames: this.anims.generateFrameNumbers('dorothy', { frames: [0, 4, 8] })
         });
         this.anims.create({
-          key: 'lionwalkback', 
-          frameRate: 6,
+          key: 'dorothyright', 
+          frameRate: 4,
           repeat: -1,
-          frames: this.anims.generateFrameNumbers('lion', { start: 0, end: 2 })
+          frames: this.anims.generateFrameNumbers('dorothy', { frames: [1, 5, 9] })
         });
         this.anims.create({
-          key: 'lionwalkright', 
-          frameRate: 6,
+          key: 'dorothyback', 
+          frameRate: 4,
           repeat: -1,
-          frames: this.anims.generateFrameNumbers('lion', { start: 3, end: 5 })
+          frames: this.anims.generateFrameNumbers('dorothy', { frames: [2, 6, 10] })
         });
         this.anims.create({
-          key: 'lionwalkleft', 
-          frameRate: 6,
+          key: 'dorothyleft', 
+          frameRate: 4,
           repeat: -1,
-          frames: this.anims.generateFrameNumbers('lion', { start: 9, end: 11 })
+          frames: this.anims.generateFrameNumbers('dorothy', { frames: [3, 7, 11] })
         });
+        this.dorothy.play('dorothyfront');
+        this.tinman.play('tinmanfront');
         this.lion.play('lionwalkfront');
-        this.slime = this.physics.add.sprite(32, 32, 'slime', 0);
-        this.anims.create({
-            key: 'jiggle', 
-            frameRate: 8,
-            repeat: -1,
-            frames: this.anims.generateFrameNumbers('slime', { start: 0, end: 1 })
-        });
-        this.slime.play('jiggle');
-        this.slime.body.setCollideWorldBounds(true);
+        this.dorothy.body.setCollideWorldBounds(true);
         this.cameras.main.setBounds(0,0,map.widthInPixels, map.heightInPixels);
-        this.cameras.main.startFollow(this.slime, true, 0.5, 0.5);
+        this.cameras.main.startFollow(this.dorothy, true, 0.5, 0.5);
         this.physics.world.bounds.setTo(0, 0, map.widthInPixels, map.heightInPixels);
         this.cursors = this.input.keyboard.createCursorKeys();
         treeLayer.setCollisionByProperty({ collides: true });
-        this.physics.add.collider(this.slime, treeLayer);
-        this.physics.add.overlap(this.slime, this.lion , collectLion, null, this);
-        this.physics.add.overlap(this.slime, this.scarecrow, collectScarecrow, null, this);
-        this.physics.add.overlap(this.slime, this.tinman, collectTinman, null, this);
+        this.physics.add.collider(this.dorothy, treeLayer);
+        this.physics.add.overlap(this.dorothy, this.lion , collectLion, null, this);
+        this.physics.add.overlap(this.dorothy, this.scarecrow, collectScarecrow, null, this);
+        this.physics.add.overlap(this.dorothy, this.tinman, collectTinman, null, this);
        let fkey = this.input.keyboard.addKey('F');
         fkey.on(
         'down', 
@@ -116,62 +167,81 @@ class Yellow extends Phaser.Scene{
     update(){
         this.direction = new Phaser.Math.Vector2(0);
         if(this.cursors.left.isDown){
+            this.dorothy.play('dorothyleft', true);
             this.direction.x = -1; 
             if(collectedLion) {
               this.lion.play('lionwalkleft', true);
             }
+            if(collectedTinman) {
+              this.tinman.play('tinmanleft', true);
+            }
         }
         else if(this.cursors.right.isDown){
+            this.dorothy.play('dorothyright', true);
             this.direction.x = 1; 
             if(collectedLion) {
               this.lion.play('lionwalkright', true);
             }
+            if(collectedTinman) {
+              this.tinman.play('tinmanright', true);
+            }
         }
         if(this.cursors.up.isDown){
+            this.dorothy.play('dorothyback', true);
             if(collectedLion) {
               this.lion.play('lionwalkfront', true);
+            }
+            if(collectedTinman) {
+              this.tinman.play('tinmanback', true);
             }
             this.direction.y = -1; 
         }
         else if(this.cursors.down.isDown){
+          this.dorothy.play('dorothyfront', true);
+          if(collectedLion) {
+            this.lion.play('lionwalkfront', true);
+          }
+          if(collectedTinman) {
+            this.tinman.play('tinmanfront', true);
+          }
             this.direction.y = 1; 
         }
         this.direction.normalize();
-        this.slime.setVelocity(this.VEL * this.direction.x, this.VEL * this.direction.y);
+        this.dorothy.setVelocity(this.VEL * this.direction.x, this.VEL * this.direction.y);
         if(collectedLion){
-            this.lion.x = this.slime.x;
-            this.lion.y = this.slime.y+100; 
+            this.lion.x = this.dorothy.x;
+            this.lion.y = this.dorothy.y+100; 
         }
         if(collectedScarecrow){
-          this.scarecrow.x = this.slime.x;
-          this.scarecrow.y = this.slime.y+20; 
+          this.scarecrow.x = this.dorothy.x;
+          this.scarecrow.y = this.dorothy.y+60; 
         }
         if(collectedTinman){
-          this.tinman.x = this.slime.x;
-          this.tinman.y = this.slime.y+30; 
+          this.tinman.x = this.dorothy.x;
+          this.tinman.y = this.dorothy.y+30; 
         }
         if(collectedScarecrow && collectedLion && collectedTinman){
-          if(this.slime.y >= 1450 && this.slime.x >= 1450){
+          if(this.dorothy.y >= 1450 && this.dorothy.x >= 1450){
             this.scene.start("monkeyScene");
           }
         }
-        console.log("Y" + this.slime.y);
-        console.log("X"  + this.slime.x);
+        console.log("Y" + this.dorothy.y);
+        console.log("X"  + this.dorothy.x);
     }
 }
 
-function collectLion(slime, lion){
+function collectLion(dorothy, lion){
     console.log("collison");
     collectedLion = true;
 }
 
-function collectScarecrow(slime, lion){
+function collectScarecrow(dorothy, lion){
   console.log("collison");
   collectedScarecrow = true;
 }
 
 
-function collectTinman(slime, lion){
+function collectTinman(dorothy, lion){
   console.log("collison");
   collectedTinman= true;
 }
