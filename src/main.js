24455dbb5fe1c07183a67b94a06b4094eb45f97f
Trevor Gardner <trevorthebike@gmail.com>
2023-06-13 12:55:38 -0700

    added more comments to main.js

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ Timers: Implemented in monkey scene to make random movement at random time inter
 Text objects: Included in every transition stage to give the player instructions and rules
             the animation manager: many animinations for charaters, including directional changing animation
 Tilemaps: multiple tilemaps for scenes with multiple layers
+
+In my game, I have added an extra level of polish to the mechanics, with unique mechanics for each scene. 
+Implementing random movement for the monkeys and witch posed a technical challenge, while the witch scene presented additional complexity with a collectible object that can be carried by the player. 
+This item enables the player to attack the witch using the space button, triggering a death animation if the witch is within a specific range. 
+Implementing this ranged attack feature proved to be challenging. 
+These innovative features demonstrate my dedication and creativity, deserving an extra grade "tilt" to acknowledge the effort put into the game.
 */
 
 let config ={
